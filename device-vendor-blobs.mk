d125077244d3d73b168ac273e8f02d9d33f0b2f6
kaijura <emailkaijura@gmail.com>
2012-03-14 14:31:45 -0700

    add Facelock libraries and build with Facelock

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -64,6 +64,32 @@ PRODUCT_COPY_FILES += \
     vendor/htc/vivo/apps/GooManager_1.5.2_signed.apk:/system/app/GooManager.apk \
     vendor/htc/vivo/apps/ApexLauncher_v1.0.2.apk:/system/app/ApexLauncher.apk
 
-# Patched libsurfaceflinger for rotating arrows credit: st.matt@xda Not needed for 4.3+ AOKP, surfaceflinger code has been changed to support
+# Facelock Libraries
+PRODUCT_COPY_FILES += \
+    vendor/kaijura/google/libfacelock_jni.so:system/lib/libfacelock_jni.so \
+    vendor/kaijura/google/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so
+
+# Facelock App
+PRODUCT_COPY_FILES += \
+    vendor/kaijura/google/FaceLock.apk:system/app/FaceLock.apk
+
+# Facelock vendor files
+PRODUCT_COPY_FILES += \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-r0-ri30.4a/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-r0-ri30.4a/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rn30-ri30.5/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rn30-ri30.5/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rp30-ri30.5/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rp30-ri30.5/full_model.bin \
+    vendor/kaijura/google/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin
+
+# Rotating softkeys by st.matt
+# Patched libsurfaceflinger Not needed for 4.3+, now working in default surfaceflinger source
 #PRODUCT_COPY_FILES += \
 #    vendor/htc/vivo/proprietary/libsurfaceflinger.so:/system/lib/libsurfaceflinger.so
